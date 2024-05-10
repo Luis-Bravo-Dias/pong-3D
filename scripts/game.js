eaf8a6cd55c5c3646e5c224668a435ee1ed24eb5
Luis Dias <kazu@pop-os.localdomain>
2024-05-10 16:10:09 +0100

    basic field done

--- scripts/game.js
+++ scripts/game.js
@@ -1,53 +1,49 @@
 //import { PointLight } from "./three.module.min";
 
-//set the scene size
+// set the scene size
 var WIDTH = 640;
-HEIGHT = 360;
+var HEIGHT = 360;
 
 // create a WebGL renderer, camera and a scene
 var renderer = new THREE.WebGLRenderer();
 
-//start the renderer
+// start the renderer
 renderer.setSize(WIDTH, HEIGHT);
 
-//attach the render-supplied DOM element (the gameCanvas)
+// attach the render-supplied DOM element (the gameCanvas)
 var c = document.getElementById("gameCanvas");
 c.appendChild(renderer.domElement);
 
-camera = new THREE.PerspectiveCamera(
-    //VIEW_ANGLE,
-    //ASPECT,
-   // NEAR,
-    //FAR
+var camera = new THREE.PerspectiveCamera(
+    45, // VIEW_ANGLE
+    WIDTH / HEIGHT, // ASPECT
+    0.1, // NEAR
+    10000 // FAR
 );
 
-scene = new THREE.Scene();
+var scene = new THREE.Scene();
 
-//add the camera to the scene
-scene.add(camera);
+// set a default position for the camera
+camera.position.z = 500;
 
-//set a default position for the camera
-camera.position.z = 320;
-
-//sphere
+// sphere
 var radius = 5,
-segments = 6,
-rings = 6;
+    segments = 6,
+    rings = 6;
 
-var sphereMaterial =
-new THREE.MeshLambertMaterial(
-    {
-        color: 0xD43001
-    }
-);
+var sphereMaterial = new THREE.MeshLambertMaterial({
+    color: 0xD43001,
+    wireframe: false
+});
 
 var ball = new THREE.Mesh(
     new THREE.SphereGeometry(radius, segments, rings),
-    sphereMaterial);
+    sphereMaterial
+);
 
 scene.add(ball);
 
-//POINT OF LIGHT
+// POINT OF LIGHT
 var pointLight = new THREE.PointLight(0xF8D898);
 
 pointLight.position.x = -1000;
@@ -58,37 +54,93 @@ pointLight.distance = 10000;
 
 scene.add(pointLight);
 
-//PLANE
-var planeMaterial =
-new THREE.MeshLambertMaterial(
-   { color: 0x4BD121}
-);
+// PLANE
+var planeWidth = 640,
+    planeHeight = 360,
+    planeQuality = 15;
 
-//ERROR HERE
+var planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0x4BD121,
+    wireframe: true
+});
+
 var plane = new THREE.Mesh(
-
-	  new THREE.PlaneGeometry(
-		planeWidth * 0.95,
-		planeHeight,
-		planeQuality,
-		planeQuality),
-
-	  planeMaterial);
+    new THREE.PlaneGeometry(
+        planeWidth * 0.95,
+        planeHeight,
+        planeQuality,
+        planeQuality
+    ),
+    planeMaterial
+);
 
 scene.add(plane);
 
-function setup()
-{
+// PADDLES
+var paddleWidth = 20,
+    paddleHeight = 60,
+    paddleDepth = 10,
+    paddleQuality = 1;
+
+var paddle1Material = new THREE.MeshLambertMaterial({
+    color: 0xD43001,
+    wireframe: false
+});
+
+var paddle2Material = new THREE.MeshLambertMaterial({
+    color: 0x33A0FF,
+    wireframe: false
+});
+
+var paddle1 = new THREE.Mesh(
+    new THREE.CubeGeometry(
+        paddleWidth,
+        paddleHeight,
+        paddleDepth,
+        paddleQuality,
+        paddleQuality,
+        paddleQuality
+    ),
+    paddle1Material
+);
+
+scene.add(paddle1);
+
+var paddle2 = new THREE.Mesh(
+    new THREE.CubeGeometry(
+        paddleWidth,
+        paddleHeight,
+        paddleDepth,
+        paddleQuality,
+        paddleQuality,
+        paddleQuality
+    ),
+    paddle2Material
+);
+
+scene.add(paddle2);
+
+// PADDLES POSITION
+paddle1.position.x = -planeWidth / 2 + paddleWidth;
+paddle2.position.x = planeWidth / 2 - paddleWidth;
+
+paddle1.position.z = paddleDepth / 2;
+paddle2.position.z = paddleDepth / 2;
+
+function setup() {
     draw();
 }
 
-function draw()
-{
-    //draw THREE.JS scene
+function draw() {
+    // draw THREE.JS scene
     renderer.render(scene, camera);
 
-    //loop the draw() function
+    // loop the draw() function
     requestAnimationFrame(draw);
+    camera.position.x = paddle1.position.x - 100;
+    camera.position.z = paddle1.position.z + 100;
+    camera.rotation.z = -90 * Math.PI/180;
+    camera.rotation.y = -60 * Math.PI/180;
 
-    //process game logic
+    // process game logic
 }
